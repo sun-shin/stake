1e459f58c229bb63bd894d8e8147c5eb65d45233
sun-shin <sunhanshin2@EMAIL.com>
2020-10-23 10:11:49 -0500

    users and sessions RESTful routes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -15,4 +15,32 @@ class Api::UsersController < ApplicationController
       render json: { errors: @user.errors.full_messages }, status: :bad_request
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+    render "show.json.jb"
+  end
+
+  def edit
+    @user = User.find(params[:id])
+
+    @user.name = params[:name] || @user.name
+    @user.email = params[:email] || @user.email
+    @user.password = params[:password] || @user.password
+    @user.password_confirmation = params[:password_confirmation] || @user.password_confirmation
+    @user.phone_number = params[:phone_number] || @user.phone_number
+    @user.image_url = params[:image_url] || @user.image_url
+
+    if @user.save
+      render "show.json.jb"
+    else
+      render json: { errors: @user.errors.full_messages }, status: 422
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: {message: "Account has been destroyed"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
+    get "/users/:id" => "users#show"
+    post "/users" => "users#create"
+    patch "/users/:id" => "users#edit"
+    delete "/users/:id" => "users#destroy"
+    
+    post "/sessions" => "sessions#create"
   end
 end
